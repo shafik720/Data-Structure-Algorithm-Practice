76f909f2987eeb2db25e4880795964ad45c842c1
shafik720 <shafikrasel5@gmail.com>
2023-05-13 22:26:27 +0600

    bug fixed

--- Linear & Binary Search/index.js
+++ Linear & Binary Search/index.js
@@ -2,22 +2,27 @@
 
 const dummyArray = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
 const targetValue = 100;
-console.log(dummyArray.length);
 
-// const binarySearch = (arr, target) => {
-//     let start = 0;
-//     let end = arr.length - 1;
-//     let mid;
+const binarySearch = (arr, target) => {
+    let start = 0;
+    let end = arr.length - 1;
+    let mid ;
 
-//     while () {
-//         mid = Math.floor((start + end) / 2);
+    // console.log(arr[end]) ; 
 
-//         if (arr[mid] === target) {
-//             return console.log('Data found at ', +arr[mid]);
-//         } else if (arr[mid] < target) {
-//             start = arr[mid] + 1;
-//         } else if (arr[mid] > target) {
-//             end = arr[mid] - 1;
-//         }
-//     }
-// }
+    while ( start <= end ) {
+        console.log('Entered in loop') ;
+        mid = Math.floor((start + end) / 2);
+
+        if (arr[mid] === target) {
+            return console.log('Data found at ', + mid);
+        } else if (arr[mid] < target) {
+            start = mid + 1;
+        } else if (arr[mid] > target) {
+            end = mid - 1;
+        }
+    }
+    return console.log('Data Not Found !') ;
+}
+
+binarySearch(dummyArray, 105) ;
