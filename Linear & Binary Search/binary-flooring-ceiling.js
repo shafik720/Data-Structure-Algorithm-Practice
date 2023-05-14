d6c4dc2bd4b2680d15ac855265194fe43b2a93ba
shafik720 <shafikrasel5@gmail.com>
2023-05-15 00:59:30 +0600

    problem bug solved

--- Linear & Binary Search/binary-flooring-ceiling.js
+++ Linear & Binary Search/binary-flooring-ceiling.js
@@ -1,34 +1,31 @@
 
 
-const dummynums = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const dummyArray = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
 
-const dummyArray = [25, 29, 45, 55, 65, 75, 85 , 105, 305];
-
-
-const binarySearch = (nums, target) => {
+const binarySearch = (arr, target) => {
     let start = 0;
-    let end = nums.length - 1;
+    let end = arr.length - 1;
     let mid;
 
-    if (target > nums[nums.length - 1] || target < nums[start]) {
-        return console.log('Data not found in array');
-    } else {
-        while (start <= end) {
-            mid = Math.floor((start + end) / 2);
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
 
-            if (nums[mid] === target) {
-                return 'Data Found at ', mid;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else if (nums[mid] > target) {
-                end = mid - 1
-            }
+        if (arr[mid] === target) {
+            return;
+        } else if (arr[mid] < target) {
+            start = mid + 1;
+        } else if (arr[mid] > target) {
+            end = mid - 1;
         }
-
-        console.log('Ceiling ', nums[mid]);
-        console.log('Flooring ', nums[mid - 1]);
+    }
+    if (arr[mid] < target) {
+        console.log('Ceiling is: ', arr[mid + 1]);
+        console.log('Flooring is: ', arr[mid]);
+    } else {
+        console.log('Ceiling is: ', arr[mid]);
+        console.log('Flooring is: ', arr[mid - 1]);
     }
     return;
 }
 
-binarySearch(dummyArray, 28);
+binarySearch(dummyArray, 27);
